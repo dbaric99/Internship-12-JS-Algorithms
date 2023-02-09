6aa488df0ac142bc189149cf36bc73ba141359e7
dbaric99 <damjana.baric1999@gmail.com>
2023-02-10 00:33:26 +0100

    Commented out test input

--- scripts/task1.js
+++ scripts/task1.js
@@ -6,16 +6,16 @@ do {
 } while(!confirm("End?"))
 
 /* TEST DATA */
-people.push({name: "Meredith", lastname: "Grey", height: 170});
-people.push({name: "Amelia", lastname: "Shepard", height: 160});
-people.push({name: "Christina", lastname: "Yang", height: 165});
-people.push({name: "Miranda", lastname: "Bailey", height: 158});
-people.push({name: "Callie", lastname: "Torres", height: 210});
-people.push({name: "Jo", lastname: "Wilson", height: 165});
-people.push({name: "Arizona", lastname: "Robinson", height: 163});
-people.push({name: "Maggie", lastname: "Pierce", height: 168});
-people.push({name: "April", lastname: "Kepner", height: 170});
-people.push({name: "Carina", lastname: "Deluca", height: 180});
+// people.push({name: "Meredith", lastname: "Grey", height: 170});
+// people.push({name: "Amelia", lastname: "Shepard", height: 160});
+// people.push({name: "Christina", lastname: "Yang", height: 165});
+// people.push({name: "Miranda", lastname: "Bailey", height: 158});
+// people.push({name: "Callie", lastname: "Torres", height: 210});
+// people.push({name: "Jo", lastname: "Wilson", height: 165});
+// people.push({name: "Arizona", lastname: "Robinson", height: 163});
+// people.push({name: "Maggie", lastname: "Pierce", height: 168});
+// people.push({name: "April", lastname: "Kepner", height: 170});
+// people.push({name: "Carina", lastname: "Deluca", height: 180});
 
 const calculateAvgHeight = () => {
     return people.reduce((sum, person) => sum+person.height, 0)/people.length;
